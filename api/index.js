14d5b0f1480fdd4c1480ee1034a7d8a7ca647d24
Eli Salamie <egsalamie@gmail.com>
2025-06-20 17:25:23 -0500

    Switch to serverless-http wrapper and simplify vercel.json

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const serverless = require("serverless-http");
 const createApp = require("../dist/index.js").default || require("../dist/index.js");
 
+// Create the app once and cache the handler
+let cachedHandler = null;
+
 module.exports = async (req, res) => {
-  const app = await createApp();
-  return serverless(app)(req, res);
+  if (!cachedHandler) {
+    const app = await createApp();
+    cachedHandler = serverless(app);
+  }
+  return cachedHandler(req, res);
 };
